40c17afa4c140b4557884e0cd97ed06792642255
Totis <totisnathor@gmail.com>
2024-11-20 19:09:09 -0600

    Se agrego los respectivos Models que se utilizaran

---
+++ src/models/capituloModel.php
@@ -0,0 +1,9 @@
+<?php 
+    class Capitulo{
+        public $id;
+        public $numero;
+        public $titulo;
+        public $id_manga;
+        public $fecha_publicacion;
+    }
+?>
---
+++ src/models/mangaModel.php
@@ -0,0 +1,11 @@
+<?php 
+    class Manga {
+        public $id;
+        public $titulo;
+        public $descripcion;
+        public $fecha_publicacion;
+        public $id_autor;
+        public $id_genero;
+        public $portada;
+    }
+?>
